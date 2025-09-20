2b5e2b60b6f32c081750f5887b8e9743d6ef10b2
George Mavroeidis <gmav.graphics@gmail.com>
2025-09-20 03:05:57 -0400

    Continuation of Vulkan Rendering Loop

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -10,27 +10,45 @@
 //bootstrap library
 #include "VkBootstrap.h"
 
+// Inneficient at scale, but good for this example.
+struct DeletionQueue
+{
+	std::deque<std::function<void()>> deletors;
+
+	void push_function(std::function<void()>&& function) {
+		deletors.push_back(function);
+	}
+
+	void flush() {
+		// Reverse iterate the deletion queue to execute all the functions
+		for (auto it = deletors.rbegin(); it != deletors.rend(); it++)
+		{
+			(*it)(); //call functors
+		}
+
+		deletors.clear();
+	}
+};
+
 struct FrameData {
 
 	VkCommandPool _commandPool;
 	VkCommandBuffer _mainCommandBuffer;
 	VkSemaphore _swapchainSemaphore, _renderSemaphore;
 	VkFence _renderFence;
+	DeletionQueue _deletionQueue;
 };
 
 constexpr unsigned int FRAME_OVERLAP = 2; // for double-buffering
 constexpr unsigned int WAIT_FENCE_TIMEOUT = 1000000000; // for double-buffering
 
-class VulkanEngine {
-
-	// --- omitted ---
-
+class VulkanEngine
+{
 	VkInstance _instance;// Vulkan library handle
 	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
 	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
 	VkDevice _device; // Vulkan device for commands
 	VkSurfaceKHR _surface;// Vulkan window surface
-
 	VkSwapchainKHR _swapchain;
 	VkFormat _swapchainImageFormat;
 
@@ -40,8 +58,15 @@ class VulkanEngine {
 
 	FrameData _frames[FRAME_OVERLAP];
 
+	VmaAllocator _allocator;
+
+	//draw resources
+	AllocatedImage _drawImage;
+	VkExtent2D _drawExtent;
+
 	VkQueue _graphicsQueue;
 	uint32_t _graphicsQueueFamily;
+	DeletionQueue _mainDeletionQueue;
 
 	void init_vulkan();
 	void init_swapchain();
@@ -71,6 +96,7 @@ public:
 
 	//draw loop
 	void draw();
+	void draw_background(VkCommandBuffer cmd);
 
 	//run main loop
 	void run();
